735d331d92a13b09728886c8963c2b1ef4040e12
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-02-09 17:05:06 -0300

    feat: testes da rota que lista os consoles.

--- tests/integrations/consoles.test.ts
+++ tests/integrations/consoles.test.ts
@@ -1,5 +1,6 @@
 import app from "app";
 import supertest from "supertest";
+import { createConsole } from "../factories/consoles-factory";
 import { cleanDb } from "../helpers";
 
 const api = supertest(app);
@@ -7,9 +8,28 @@ const api = supertest(app);
 beforeEach(async () => await cleanDb());
 
 describe("GET /consoles", () => {
-    it("", async ()=>{
-        
-    })
+  it("Should respond status 200 and list of consoles", async () => {
+    const consoleCreated = await createConsole();
+
+    const result = await api.get("/consoles");
+
+    expect(result.status).toBe(200);
+    expect(result.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: consoleCreated.id,
+          name: consoleCreated.name,
+        }),
+      ])
+    );
+  });
+
+  it("Should respond status 200 and consoles list empty", async () => {
+    const result = await api.get("/consoles");
+
+    expect(result.status).toBe(200);
+    expect(result.body).toEqual([]);
+  });
 });
 
 describe("GET /consoles/:id", () => {});
