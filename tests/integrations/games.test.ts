1ce173e5b7064bbce06aff982a1f970974b9de40
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-02-09 19:40:50 -0300

    feat: testes da rota que incere um game

--- tests/integrations/games.test.ts
+++ tests/integrations/games.test.ts
@@ -1,5 +1,6 @@
 import faker from "@faker-js/faker";
 import app from "app";
+import prisma from "config/database";
 import supertest from "supertest";
 import { createConsole } from "../factories/consoles-factory";
 import { createGame } from "../factories/games-factory";
@@ -59,5 +60,56 @@ describe("GET /games/:id", () => {
       title: gameCreated.title,
       consoleId: gameCreated.consoleId,
     });
+  });
+});
+
+describe("POST /games", () => {
+  it("Should respond status 409 if the game is already registered in the database", async () => {
+    const gameCreated = await createGame();
+
+    const result = await api
+      .post("/games")
+      .send({ title: gameCreated.title, consoleId: gameCreated.id });
+
+    expect(result.status).toBe(409);
+  });
+
+  it("Must respond status 409 if consoloId does not exist", async () => {
+    const title = faker.name.firstName();
+
+    const result = await api.post("/games").send({ title, consoleId: 0 });
+
+    expect(result.status).toBe(409);
+  });
+
+  it("Should respond status 422 if game title is in an invalid format", async () => {
+    const consoleCreated = await createConsole();
+    const result = await api
+      .post("/games")
+      .send({ title: 1223, consoleId: consoleCreated.id });
+
+    expect(result.status).toBe(422);
+  });
+
+  it("Should respond status 201 if the games was successfully created in the database", async () => {
+    const consoleCreated = await createConsole();
+    const title = faker.name.firstName();
+
+    const result = await api
+      .post("/games")
+      .send({ title, consoleId: consoleCreated.id });
+
+    const sideEffect = await api.get(`/games`);
+
+    expect(result.status).toBe(201);
+    expect(sideEffect.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          title,
+          consoleId: consoleCreated.id,
+        }),
+      ])
+    );
   });
 });
